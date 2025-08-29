1a365bc3e43832bf0aa15f99728579062a1ca723
Tarun Mahajan <mahajan.tarun1990@gmail.com>
2025-08-29 20:04:06 +0530

    feat: add safe color methods with red fallback
    - Add safe_color_info, safe_color_name, safe_css_class methods
    - Add convenience methods for current product color
    - Update views to use safe color methods instead of direct hash access
    - Ensure red is used as default when color is not in available_colors
    - Prevent errors when product.color is invalid or missing

--- app/models/concerns/color_enum.rb
+++ app/models/concerns/color_enum.rb
@@ -49,6 +49,26 @@ module ColorEnum
       def name_for_color(color_value)
         AVAILABLE_COLORS[color_value.to_sym]&.dig(:name) || color_value.humanize
       end
+
+      def safe_color_info(color_value)
+        # Return color info if available, otherwise return red as default
+        color_value = color_value.to_s if color_value
+        if color_value && AVAILABLE_COLORS[color_value.to_sym]
+          AVAILABLE_COLORS[color_value.to_sym]
+        else
+          AVAILABLE_COLORS[:red] # Default to red
+        end
+      end
+
+      def safe_color_name(color_value)
+        # Return color name if available, otherwise return red
+        safe_color_info(color_value)[:name]
+      end
+
+      def safe_css_class(color_value)
+        # Return CSS class if available, otherwise return red
+        safe_color_info(color_value)[:css_class]
+      end
     end
   end
 
@@ -59,5 +79,30 @@ module ColorEnum
 
   def name_for_color(color_value)
     self.class.name_for_color(color_value)
+  end
+
+  def safe_color_info(color_value)
+    self.class.safe_color_info(color_value)
+  end
+
+  def safe_color_name(color_value)
+    self.class.safe_color_name(color_value)
+  end
+
+  def safe_css_class(color_value)
+    self.class.safe_css_class(color_value)
+  end
+
+  # Convenience methods for current product color
+  def safe_current_color_info
+    safe_color_info(self.color)
+  end
+
+  def safe_current_color_name
+    safe_color_name(self.color)
+  end
+
+  def safe_current_css_class
+    safe_css_class(self.color)
   end
 end
